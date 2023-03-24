535cb29c4749bae8f6ab1f154080d2f5e39c7572
polyhistor2050 <chrisspaul2050@gmail.com>
2023-03-24 23:55:08 +0300

    updated to file

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ let loops = 0;
 let peopleVisible = false;
 function gameLoop() {
 	peopleVisible = !peopleVisible;
+	flashCharacters();
 	loops++;
 	if (loops < 12){
 		setTimeout(gameLoop, 3000);
 	}else {
 		alert("Game over!");
+	}
+}
+
+function flashCharacters(){
+	let board = document.getElementById("board");
+	let classToSet = peopleVisible ? "character visible" : "character hidden";
+	for(let index = 0; index < 6; index++) {
+		board.children[index].className = classToSet;
 	}
 }
 	
